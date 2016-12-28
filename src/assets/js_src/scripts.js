e54a87015984d704a5f5e496e63bbbe7121ad268
Benjamin Hollway <benjamin@nothingrandom.com>
2016-12-28 11:52:43 +0000

    JS function for click events on the services elm

--- src/assets/js_src/scripts.js
+++ src/assets/js_src/scripts.js
@@ -1,1 +1,16 @@
-var $ = require('jquery'); // ^2.1.4 included
+var $ = require('jquery'); // ^3.1.1 included
+
+$(function() {
+	$('.nav--services li').on('click', function(e) {
+		e.preventDefault();
+
+		var t = $(this),
+		id = t.attr('id');
+
+		$('.nav--services li').removeClass('current');
+		t.addClass('current');
+
+		$('.services--text').removeClass('show').addClass('hidden');
+		$('.services--text#' + id).removeClass('hidden').addClass('show');
+	});
+});
