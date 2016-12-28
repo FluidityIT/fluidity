a5009940914c2d59d3ca8ffc96cd2f25961fcdd4
Benjamin Hollway <benjamin@nothingrandom.com>
2016-12-28 11:47:55 +0000

    Add ids to header items

--- src/inc/header.php
+++ src/inc/header.php
@@ -6,10 +6,10 @@
 			</div>
 			<nav class="nav nav--main">
 				<ul>
-					<li class="one selected"><a href="#home" title="">Home</a></li>
-					<li class="two"><a href="#services" title="">Services</a></li>
-					<li class="three"><a href="#about" title="">About</a></li>
-					<li class="four"><a href="#contact" title="">Contact</a></li>
+					<li id="index" class="nav__item one selected"><a href="#home" title="">Home</a></li>
+					<li id="services" class="nav__item two"><a href="#services" title="">Services</a></li>
+					<li id="about" class="nav__item three"><a href="#about" title="">About</a></li>
+					<li id="contact" class="nav__item four"><a href="#contact" title="">Contact</a></li>
 					<hr />
 				</ul>
 			</nav>
