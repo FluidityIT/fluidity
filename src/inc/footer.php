0dcf2184885ad8cdaec116ef8d5db93cd57ccf44
Benjamin Hollway <benjamin@nothingrandom.com>
2017-01-10 12:38:56 +0000

    Add linkedin logo

--- src/inc/footer.php
+++ src/inc/footer.php
@@ -7,6 +7,7 @@
 						<!--li><a href=""><i class="fa fa-linkedin"></i></a></li-->
 						<li><a href="https://www.instagram.com/fluidityit/"><i class="fa fa-instagram"></i></a></li>
 						<li><a href="https://medium.com/@nothingrandom"><i class="fa fa-medium"></i></a></li>
+						<li><a href="https://www.linkedin.com/in/danielmcnaught"><i class="fa fa-linkedin"></i></a></li>
 						<li><a href="tel:+441273974994"><i class="fa fa-phone"></i></a></li>
 						<!--li><a href="mailto:hello@fluidity.it"><i class="fa fa-envelope-o"></i></a></li-->
 					</ul>
